09e0290657f2e96cb5801dceef530985526ee24d
Branden Bird <brandenjbird@gmail.com>
2017-07-21 11:14:54 -0600

    Seperate initializer methode

--- letter.rb
+++ letter.rb
@@ -4,37 +4,39 @@ require 'colorize'
 
 class RepeatLetters
 	def initialize
-		puts "Please enter a Sentence"
-		puts "press q to quit:"
+		puts "Please enter a Sentence or press q to quit:"
 
 		input = gets.strip.to_s
-
 		CheckInput.new(input)
-
 		if !input.empty?
-			most_repeated = ['', 0]
-
-			input.split(' ').each do |word|
-				@repeated = 1
-				
-				word.each_char.find do |c|
-					@repeated = word.count(c) > @repeated ? word.count(c) : @repeated
-				end
-
-				if @repeated === most_repeated[1]
-					most_repeated.push(word, @repeated)
-				elsif @repeated > most_repeated[1]
-					most_repeated = [word, @repeated]
-				end
-			end
-
-			puts most_repeated
-
+			find_repeated input
 		else
 			RepeatLetters.new
 		end
-
 	end
+
+	def find_repeated(input)
+		most_repeated = ['', 0]
+
+		input.split(' ').each do |word|
+			@repeated = 1
+
+			word.each_char do |c|
+				@repeated = word.count(c) > @repeated ? word.count(c) : @repeated
+			end
+
+			if @repeated === most_repeated[1]
+				most_repeated.push(word, @repeated)
+			elsif @repeated > most_repeated[1]
+				most_repeated = [word, @repeated]
+			end
+
+		end
+
+		puts most_repeated
+		RepeatLetters.new
+	end
+
 end
 
 RepeatLetters.new
